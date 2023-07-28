341061bf9a08b4ba89525d27f7fc7aa51dd1b3b0
Al Asaad <alahmadgaid@gmail.com>
2023-07-28 21:18:08 +0800

    fix author sequence

--- src/routes/papers/+page.svelte
+++ src/routes/papers/+page.svelte
@@ -1,20 +1,21 @@
 <script>
-	import { papers } from "$lib/data/papers.js"
-	
+	import { papers } from '$lib/data/papers.js';
+
 	let theses;
 	let nonTheses;
 	theses = papers.filter((object) => {
-		return object["type"] === "thesis"
-	})
+		return object['type'] === 'thesis';
+	});
 	nonTheses = papers.filter((object) => {
-		return object["type"] !== "thesis"
-	})
-	let data = {}; let i;
+		return object['type'] !== 'thesis';
+	});
+	let data = {};
+	let i;
 	for (i = 0; i < nonTheses.length; ++i) {
-		data[nonTheses[i]["year"]] = []
+		data[nonTheses[i]['year']] = [];
 	}
 	for (i = 0; i < nonTheses.length; ++i) {
-		data[nonTheses[i]["year"]].push(nonTheses[i])
+		data[nonTheses[i]['year']].push(nonTheses[i]);
 	}
 	let years = Object.keys(data).reverse();
 </script>
@@ -25,64 +26,72 @@
 </svelte:head>
 
 <div class="text-column">
-	<h1>Theses, Conference, & Journal Publications</h1><br><br>
+	<h1>Theses, Conference, & Journal Publications</h1>
+	<br /><br />
 	<div class="title-emphasis">Theses</div>
 	<ul>
 		{#each theses as thesis, i}
 			<li>
-				<u>{ thesis["author"] }</u> 
-				({ thesis["year"] }). 
-				{ thesis["title"] }. 
-				<span class="emphasize">{ thesis["publisher"] }</span>. 
-				(<a href="{ thesis["href"] }">read</a>)
-				{ thesis["application"] }
-			</li><br>
+				<u>{thesis['author']}</u>
+				({thesis['year']}).
+				{thesis['title']}.
+				<span class="emphasize">{thesis['publisher']}</span>. (<a href={thesis['href']}>read</a>)
+				{thesis['application']}
+			</li>
+			<br />
 		{/each}
 	</ul>
 	{#each years as year, i}
-		<div class="title-emphasis">{ year }</div>
+		<div class="title-emphasis">{year}</div>
 		<ul>
 			{#each data[year] as paper, i}
 				<li>
-					{#if paper["author"].length > 1}
-						{#each paper["author"] as author, i}
-							{#if author.includes("A.-A.B.")}
-								<u>{ author }</u>
+					{#if paper['author'].length > 1}
+						{#each paper['author'] as author, k}
+							{#if author.includes('A.-A.B.')}
+								<u>{author}</u>
 							{:else}
-								{ author }
-							{/if},&nbsp;
+								{author}
+							{/if}
+							{#if k < paper['author'].length - 2}
+								,&nbsp;
+							{:else if k === paper['author'].length - 2}
+								{#if paper["author"].length === 2}
+									&&nbsp;
+								{:else}
+									,&&nbsp;
+								{/if}
+							{/if}
 						{/each}
 					{:else}
-						<u>{ paper["author"] }</u>
+						<u>{paper['author']}</u>
 					{/if}
-					({ paper["year"] }). 
-					{ paper["title"] }. 
+					({paper['year']}).
+					{paper['title']}.
 					<span class="emphasize">
-						{#if paper["type"] === "journal"}
-							<i>{ paper["journal"] }</i>
+						{#if paper['type'] === 'journal'}
+							<i>{paper['journal']}</i>
 						{:else}
-							In <i>{ paper["proceeding"] }</i>, 
+							In <i>{paper['proceeding']}</i>,
 						{/if}
 					</span>
-					{#if paper["type"] === "journal"}
-						<b>{ paper["issue"] }</b>, 
-						{ paper["pages"] },
-						doi: { paper["doi"] }
+					{#if paper['type'] === 'journal'}
+						<b>{paper['issue']}</b>,
+						{paper['pages']}, doi: {paper['doi']}
 					{/if}
-					{#if paper["type"] === "conference"}
-						{#if paper["pages"] !== undefined}
-							{ paper["pages"] },&nbsp;
+					{#if paper['type'] === 'conference'}
+						{#if paper['pages'] !== undefined}
+							{paper['pages']},&nbsp;
 						{/if}
-						{ paper["address"] }.
-						{#if paper["publisher"] !== undefined}
-							{ paper["publisher"] }
+						{paper['address']}.
+						{#if paper['publisher'] !== undefined}
+							{paper['publisher']}
 						{/if}
 					{/if}
-					(<a href="{ paper["href"] }">read</a>)
-					{ paper["application"] }<br><br>
+					(<a href={paper['href']}>read</a>)
+					{paper['application']}<br /><br />
 				</li>
 			{/each}
-
 		</ul>
 	{/each}
 </div>
