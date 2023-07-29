d8e745544c9f8f8fdcc7ee5de0368623742c3bc2
Al Asaad <alahmadgaid@gmail.com>
2023-07-29 08:06:55 +0800

    fix welcome message

--- src/routes/WelcomeMessage.svelte
+++ src/routes/WelcomeMessage.svelte
@@ -1,6 +1,6 @@
 <div class="welcome-message">
-    I'm <b><a href="https://www.linkedin.com/in/al-ahmadgaid-asaad-68613a44/">Al-Ahmadgaid "Al" B. Asaad</a></b>, a Statistician, Data Scientist, and Researcher from the Philippines currently working as Manager of the Advanced Analytics team of 
-    <a href="https://www.jnj.com/">Johnson & Johnson</a>. Outside work, I love free diving, and reading humanities books on History, Religion, and Philosophy.
+    Assalamu 'Alaikum! I'm <b><a href="https://www.linkedin.com/in/al-ahmadgaid-asaad-68613a44/">Al-Ahmadgaid "Al" B. Asaad</a></b>, a Statistician, Data Scientist, and Researcher from the Philippines currently working as Manager of the Advanced Analytics team of 
+    <a href="https://www.jnj.com/">Johnson & Johnson</a>.
 </div>
 
 <style>
